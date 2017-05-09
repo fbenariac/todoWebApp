60b6e24ba58b26c4f29afcd56e0830b657f25af5
Fabien B <fabien.benariac@gmail.com>
2017-05-09 08:14:46 +0200

    add test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  completed   :boolean
+#  order       :integer
+#  due_date    :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
