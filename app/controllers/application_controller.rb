5fa242c5ffb2f426203c3a01fab87b7d93a29e25
Fabien B <fabien.benariac@gmail.com>
2017-05-09 08:19:09 +0200

    feat(makeup):   - Add bootstrap   - Set sass   - set templates   - ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  # Global layout for app
+  layout "application_two_cols"
+
+  # Any routes that aren't defined above here go to the 404
+  def routing_error
+    render :file => 'public/404.html', :status => :not_found, :layout => false
+  end
+  
+  # Session user tools' helper
+  helper_method :current_user
+  
+  private
+  
+    # return the session user ( or nil )
+    def current_user
+      @current_user ||= User.find( session[:user_id] ) if session[:user_id]
+    end
+   
+    # Check is current_user else redirect to login page.
+    def verify_logged_in
+      unless current_user
+        redirect_to login_path
+      end
+    end
+
 end
