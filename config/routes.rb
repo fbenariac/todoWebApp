f00e617c9cc48ff27f0ced47810396cbed2b6702
Fabien B <fabien.benariac@gmail.com>
2017-08-04 19:50:06 +0200

    fix sessions test and code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
 
+  match 'home', to: 'tasks#index', via: :all
+
   match 'login',   to: 'sessions#new',     via: :get
   match 'logout',  to: 'sessions#destroy', via: :get
   match 'sign_in', to: 'sessions#create',  via: :post
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,54 @@
-require 'test_helper'
+# require 'test_helper'
+#
+# class SessionsControllerTest < ActionDispatch::IntegrationTest
+#
+#   # test "should get new" do
+#   #   get sessions_new_url
+#   #   assert_response :success
+#   # end
+#
+#   # test "should get create" do
+#   #   get sessions_create_url
+#   #   assert_response :success
+#   # end
+#
+#   # test "should get destroy" do
+#   #   get sessions_destroy_url
+#   #   delete "/sessions/#{@session.id}"
+#   #   assert_response :success
+#   # end
+#
+# end
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
-    assert_response :success
+require "test_helper"
+
+class SessionControllerTest < ActionDispatch::IntegrationTest
+
+  test 'login session' do
+    # get the login page
+    get "/login"
+    assert_equal 200, status
+
+    # post the login and follow through to the home page
+    post "/sessions", params: {
+      username: 'john@doe.com',
+      password: 'pswd'
+    }
+
+    #follow_redirect!
+    assert_equal 200, status
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
-  end
+  test 'create session' do
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+    post "/sessions", params: {
+      email: 'john@doe.com',
+      password: 'pswd'
+    }
+
+    follow_redirect!
+    assert_equal 200, status
+
   end
 
 end
