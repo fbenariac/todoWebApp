edd98001da0f9b603c6882a6bcf81ae607aded2d
Fabien B <fabien.benariac@gmail.com>
2017-08-14 12:05:33 +0200

    set defaults value for user roles.

--- db/migrate/20170814094204_add_roles_to_users.rb
+++ db/migrate/20170814094204_add_roles_to_users.rb
@@ -1,7 +1,8 @@
+# 20170814094204_add_roles_to_users
 class AddRolesToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :admin, :boolean
-    add_column :users, :user, :boolean
-    add_column :users, :guest, :boolean
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :user, :boolean, default: false
+    add_column :users, :guest, :boolean, default: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 20170814094204) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
-    t.boolean "user"
-    t.boolean "guest"
+    t.boolean "admin", default: false
+    t.boolean "user", default: false
+    t.boolean "guest", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
