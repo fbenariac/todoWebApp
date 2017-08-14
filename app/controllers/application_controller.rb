c4fd60b4938ad2b028d5ba39adcc62360cbe69d0
Fabien B <fabien.benariac@gmail.com>
2017-08-14 12:07:01 +0200

    implement CanCanCan access strategies.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,11 @@ class ApplicationController < ActionController::Base
 
   def home; end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to main_app.root_url, :alert => exception.message }
+    end
+  end
+
 end
