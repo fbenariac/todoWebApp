08afa75b8ff9d3d947c3883dc1ae17d5167bdbae
Fabien B <fabien.benariac@gmail.com>
2017-08-20 01:01:50 +0200

    fix tests.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -2,12 +2,20 @@ require "test_helper"
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
+  include Devise::Test::IntegrationHelpers
+
   test 'should get home' do
     get '/home'
     assert_response :success
   end
 
+  test 'should redirect to login' do
+    get '/notFoundPage'
+    assert_redirected_to new_user_session_path
+  end
+
   test 'should get a routing error' do
+    sign_in users(:one)
     get '/notFoundPage'
     assert_response 404
   end
