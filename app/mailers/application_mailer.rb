7300fd057efa7bbeed55f16c54841ea609060df8
Fabien B <fabien.benariac@gmail.com>
2018-05-09 20:09:58 +0200

    add a welcome email.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,4 +4,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
+  def welcome_email(user)
+    email_to = user[:email]
+    subject = 'Welcome'
+    body = "Welcome #{user[:username]} !"
+    mail(to: email_to, subject: subject, body: body)
+  end
+
 end
